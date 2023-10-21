b6f8f20d71672f50e8d899c5057a2dc61bf63616
Son-Gyeongi <thsruddl77@gmail.com>
2023-10-21 23:54:10 +0900

    데이터 접근 기술 (시작) - 프로젝트 구조 설명 (ItemRepositoryTest/save()메서드)

--- src/test/java/hello/itemservice/repository/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/repository/ItemRepositoryTest.java
@@ -6,8 +6,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ItemRepositoryTest {
 
+    @Autowired
+    ItemRepository itemRepository;
+    @AfterEach
+    void afterEach() {
+        //MemoryItemRepository 의 경우 제한적으로 사용
+        if (itemRepository instanceof MemoryItemRepository) {
+            ((MemoryItemRepository) itemRepository).clearStore();
+        }
+    }
     @Test
     void save() {
+        //given
+        Item item = new Item("itemA", 10000, 10);
+        //when
+        Item savedItem = itemRepository.save(item);
+        //then
+        Item findItem = itemRepository.findById(item.getId()).get();
+        assertThat(findItem).isEqualTo(savedItem);
     }
 
     @Test
